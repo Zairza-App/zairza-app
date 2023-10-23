4db85524990b5c87d6eb19b7a5bbe4418be0f88b
Abhinaba Dash <abhinabadash19@gmail.com>
2023-10-23 19:14:36 +0530

    removed old card details

---
+++ lib/screens/card_details.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CardDetails extends StatelessWidget {
+  const CardDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(),
+    );
+  }
+}
