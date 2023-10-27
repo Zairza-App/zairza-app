a3b03852bacdafdf9a04c65f4b9b478a3d87e12f
Satyajitpanda17 <satyajitpanda1704@gmail.com>
2023-10-27 17:42:52 +0530

    seeMoreRoadmaps

--- lib/screens/resources/see_more_roadmaps.dart
+++ lib/screens/resources/see_more_roadmaps.dart
@@ -1,10 +1,141 @@
 import 'package:flutter/material.dart';
+import 'package:zairza_app/constants/global_variables.dart';
+import 'package:zairza_app/common/widgets/roadmap_tag.dart';
 
 class SeeMoreRoadMaps extends StatelessWidget {
   const SeeMoreRoadMaps({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: height * 0.09451,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.close),
+          color: Colors.black,
+        ),
+        title: Padding(
+          padding: EdgeInsets.symmetric(horizontal: width * 0.01860465116),
+          child: Text(
+            'Roadmaps',
+            style: GlobalVariables.textBold_24.copyWith(color: Colors.black),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Image.asset('assets/images/resources_largeBG.png'),
+                Padding(
+                  padding: EdgeInsets.all(width * 0.03381395343),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: height * 0.0001),
+                        child: Row(
+                          children: [
+                            const RoadMapTag(text: 'Web Development'),
+                            SizedBox(width: width * 0.02325581395),
+                            const RoadMapTag(text: 'App Development'),
+                            SizedBox(width: width * 0.02325581395),
+                            const RoadMapTag(text: 'CP'),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: height * 0.01),
+                      Row(
+                        children: [
+                          const RoadMapTag(text: 'AI/ML'),
+                          SizedBox(width: width * 0.02325581395),
+                          const RoadMapTag(text: 'Game Development'),
+                          SizedBox(width: width * 0.02325581395),
+                          const RoadMapTag(text: 'ROS'),
+                        ],
+                      ),
+                      SizedBox(height: height * 0.01),
+                      Row(
+                        children: [
+                          const RoadMapTag(text: 'UI/UX'),
+                          SizedBox(width: width * 0.02325581395),
+                          const RoadMapTag(text: 'CP'),
+                          SizedBox(width: width * 0.02325581395),
+                          const RoadMapTag(text: 'Web Development'),
+                          SizedBox(width: width * 0.02325581395),
+                          const RoadMapTag(text: 'Linux'),
+                        ],
+                      ),
+                      SizedBox(height: height * 0.01),
+                      Row(
+                        children: [
+                          const RoadMapTag(text: 'AI/ML'),
+                          SizedBox(width: width * 0.02325581395),
+                          const RoadMapTag(text: 'Game Development'),
+                          SizedBox(width: width * 0.02325581395),
+                          const RoadMapTag(text: 'ROS'),
+                        ],
+                      ),
+                      SizedBox(height: height * 0.01),
+                      Row(
+                        children: [
+                          const RoadMapTag(text: 'Web Development'),
+                          SizedBox(width: width * 0.02325581395),
+                          const RoadMapTag(text: 'UI/UX'),
+                          SizedBox(width: width * 0.02325581395),
+                          const RoadMapTag(text: 'App Dev'),
+                        ],
+                      ),
+                      SizedBox(height: height * 0.01),
+                      Row(
+                        children: [
+                          const RoadMapTag(text: 'AI/ML'),
+                          SizedBox(width: width * 0.02325581395),
+                          const RoadMapTag(text: 'ROS'),
+                          SizedBox(width: width * 0.02325581395),
+                          const RoadMapTag(text: 'Game Development'),
+                          SizedBox(width: width * 0.02325581395),
+                          const RoadMapTag(text: 'Linux'),
+                        ],
+                      ),
+                      SizedBox(height: height * 0.01),
+                      Row(
+                        children: [
+                          const RoadMapTag(text: 'Web Development'),
+                          SizedBox(width: width * 0.02325581395),
+                          const RoadMapTag(text: 'App Development'),
+                          SizedBox(width: width * 0.02325581395),
+                          const RoadMapTag(text: 'CP'),
+                        ],
+                      ),
+                      SizedBox(height: height * 0.01),
+                      Row(
+                        children: [
+                          const RoadMapTag(text: 'ROS'),
+                          SizedBox(width: width * 0.02325581395),
+                          const RoadMapTag(text: 'CP'),
+                          SizedBox(width: width * 0.02325581395),
+                          const RoadMapTag(text: 'UI/UX'),
+                          SizedBox(width: width * 0.02325581395),
+                          const RoadMapTag(text: 'Game Development'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
